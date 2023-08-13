fe095fb3ec88510d0e62e89a7449fe4a14201c59
mirkka29 <mirkka_92@alumni.ubc.ca>
2023-08-13 22:47:07 +0200

    creation

---
+++ mp_boxplots_phloem_samples.R
@@ -0,0 +1,47 @@
+#install.packages("ggplot")
+library(readxl)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(ggplot2)
+
+archivo_csv <- "phloem_grouped_data.csv"
+
+# Leer el archivo CSV
+d <- read.csv(archivo_csv)
+
+height<- as.factor(d$Height_cm)
+treatment <- as.factor(d$Treatment)
+
+
+# Create a customized boxplot 1_verbenone_glucoside_mg
+ggplot(d, aes(x = Treatment, y = Peak_area_1_verbenone_glucoside_mg,
+                 fill = factor(Treatment))) +
+  geom_boxplot() +
+  labs(title = "Boxplot (1) Verbenone-glucoside",
+       x = "Treatment",
+       y = "Area of peak") +
+  scale_fill_discrete(name = "Treatments") +
+  theme_minimal()
+
+# Create a customized boxplot 2_verbenone_glucoside_mg
+ggplot(d, aes(x = Treatment, y = Peak_area_2_verbenone_glucoside_mg,
+              fill = factor(Treatment))) +
+  geom_boxplot() +
+  labs(title = "Boxplot (2) Verbenone-glucoside",
+       x = "Treatment",
+       y = "Area of peak") +
+  scale_fill_discrete(name = "Treatments") +
+  theme_minimal()
+
+# Create a customized boxplot OH_verbenone
+ggplot(d, aes(x = Treatment, y = Peak_area_OH_verbenone_mg,
+              fill = factor(Treatment))) +
+  geom_boxplot() +
+  labs(title = "Boxplot OH-Verbenone",
+       x = "Treatment",
+       y = "Area of peak") +
+  scale_fill_discrete(name = "Treatments") +
+  theme_minimal()
+
